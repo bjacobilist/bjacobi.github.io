b620c49b614a06958508582115b1b094b50d0f91
BrynnJacobi <89175531+BrynnJacobi@users.noreply.github.com>
2021-12-09 21:34:03 -0600

    Update script.js

--- script.js
+++ script.js
@@ -276,7 +276,7 @@ if (arrayLength > 0)
 //Week 14 Remove Share Button and Sharelist if arraylist contains values 
   document.getElementById("sharebutton").innerHTML = ' ';
     document.getElementById("sharelist").innerHTML = ' ';
-	document.getElementById("Empty").innerHTML = '/nYay! Nothing to do!';
+	document.getElementById("Empty").innerHTML = '\nYay! Nothing to do!';
     }
 }
 
